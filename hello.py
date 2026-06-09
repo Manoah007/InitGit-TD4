a640157ba51e884ddaae7ffee4f22b37736b2b1b
b_malik <malik.boussaidi@etu.u-pec.fr>
2026-06-09 14:38:32 +0200

    Modification du message (étape B)

--- hello.py
+++ hello.py
@@ -2,7 +2,11 @@ from colorama import init , Fore , Style
 init ()
 
 def display () :
+<<<<<<< HEAD
     message = f"{ Fore . GREEN } Ceci est une { Fore . CYAN } modification { Style . RESET_ALL}"    
+=======
+    message = f"{ Fore . GREEN } Bonjour { Fore . CYAN } tout le monde !{ Style . RESET_ALL }"
+>>>>>>> 0533178 (Modification du message (étape B))
     print ( message )
 
 if __name__ == " __main__ ":
